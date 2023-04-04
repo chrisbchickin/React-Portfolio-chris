c6f5bc383e207bdfa4676f7a1520b56bbba962ca
Chrisbchicken <chrsbnjmn@gmail.com>
2023-04-03 21:25:54 -0500

    initial commit

---
+++ my-portfolio/src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Aspiring Software Engineer with a demonstrated history of working on web applications and projects. Deep understanding and technical expertise in Computer Science fundamentals, Javascript, ReactJS, NodeJS, MongoDB, CSS, HTML and other technologies.
+      </p>
+    </div>
+  );
+}
